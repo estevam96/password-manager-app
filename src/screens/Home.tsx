058dbd34006bc38e81bc7fad1d55ba65f387fe9a
Marcelo Estevam <marcelo.estevam15@hotmail.com>
2024-01-20 17:17:23 -0300

    chore: config nativewind.

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-400">
+      <Text className="text-xl font-bold ">Home</Text>
+    </View>
+  );
+}
